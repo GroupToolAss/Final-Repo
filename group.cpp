2e2ad52feb3ba7d7368661a29ffe240aeb62483d
EyosiyasAligas <105386232+EyosiyasAligas@users.noreply.github.com>
2022-05-12 00:23:44 +0300

    Update group.cpp

--- group.cpp
+++ group.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 
     cout<<"abeselom solomon......................1200738";
-    cout<<"abeselom solomon......................1201635";
+    cout<<"Eyosiyas Aligas......................1201635";
     
     return 0;
 }
