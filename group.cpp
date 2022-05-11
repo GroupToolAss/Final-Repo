25e45020a91de97bbdad2b4bab490c0c7c21da22
EyosiyasAligas <105386232+EyosiyasAligas@users.noreply.github.com>
2022-05-12 00:22:48 +0300

    Update group.cpp
    Version 1.2

--- group.cpp
+++ group.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main(){
 
     cout<<"abeselom solomon......................1200738";
-
+    cout<<"abeselom solomon......................1201635";
+    
     return 0;
 }
 
