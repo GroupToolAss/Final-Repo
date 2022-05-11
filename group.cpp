9e7de87a15275a2452e5127a8cffe3f3b931e092
Hana-Belay <104908230+Hana-Belay@users.noreply.github.com>
2022-05-12 00:38:03 +0300

    Update group.cpp

--- group.cpp
+++ group.cpp
@@ -5,7 +5,8 @@ int main(){
     cout<<"abeselom solomon......................1200738";
     cout<<"Eyosiyas Aligas......................1201635";
     cout<<"dereje getachew.......................1201438";
-    
+    cout<<"Hana Belay............................1207363";
     return 0;
 }
+
 
