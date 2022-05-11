b75e21ab2acf71ab6ef4357779522d5c2c1aecec
yeheyagit1 <103607911+yeheyagit1@users.noreply.github.com>
2022-05-12 00:26:45 +0300

    Update group.cpp

--- group.cpp
+++ group.cpp
@@ -4,6 +4,7 @@ int main(){
 
     cout<<"abeselom solomon......................1200738";
     cout<<"Eyosiyas Aligas......................1201635";
+    cout<<"dereje getachew.......................1201438";
     
     return 0;
 }
